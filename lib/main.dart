c6589c3c3137848b83fbb6113e7d36e5d7eb11c4
sujalutekar <sujalisosm@gmail.com>
2024-02-10 16:46:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:face_detection_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Face Detection',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const FaceDetectorView(),
+    );
+  }
+}
